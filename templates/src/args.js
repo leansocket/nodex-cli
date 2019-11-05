713bd3938077fab623c6c728c8c584937919bd06
chengqilin <qilin.cheng@foxmail.com>
2019-11-06 01:37:27 +0800

    update the project template

--- templates/src/args.js
+++ templates/src/args.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const makeArgsPath = function () {
     return path.join(process.cwd(), `data/args-${process.env.NODE_ENV}.json`);
 };
 
 const argsPath = makeArgsPath();
-
-if (fs.existsSync(argsPath)) {
-    let data_args = require(argsPath);
-    for (let k in data_args) {
-        exports[k] = data_args[k];
-    }
-}
-else {
+if (!fs.existsSync(argsPath)) {
     console.log(`ignored args file: '${argsPath}' because it is not found.`);
+    return;
+}
+    
+let data_args = require(argsPath);
+for (let k in data_args) {
+    exports[k] = data_args[k];
 }
