260f6c3d7928fb6f17606165a6ce2229b3afcfed
Qilin <qilin.cheng@foxmail.com>
2019-11-13 00:04:06 +0800

    0.1.2

--- templates/src/data.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const libs = require('nodex-libs');
-
-exports.init = (args) => { };
--- templates/src/logic.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const libs = require('nodex-libs');
-
-exports.init = (args) => { };
-
-exports.helloWorld = async () => {
-    return 'hello world!';
-};
