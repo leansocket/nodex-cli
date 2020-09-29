8f0e9da57730537149b229bb47721fb17be50a04
ruby.luo <ruby.luo@xgimi.com>
2020-09-29 12:48:04 +0800

    refactor: 重构init命令

---
+++ src/index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const { program } = require("commander");
+const { version, bin } = require("../package.json");
+const path = require("path");
+const chalk = require("chalk");
+
+const [cmd] = Object.keys(bin);
+program.version(version);
+
+const mapActions = {
+  init: {
+    alias: "i",
+    desc: "init a project!",
+    examples: [`${cmd} init <projectName>`],
+  },
+};
+
+Object.keys(mapActions).forEach((key) => {
+  program
+    .command(key)
+    .alias(mapActions[key].alias)
+    .description(mapActions[key].desc)
+    .action(() =>
+      require(path.resolve(__dirname, `./actions/${key}`))(
+        ...process.argv.slice(3)
+      )
+    )
+    .on("--help", () => {
+      console.log("");
+      console.log("Example:");
+      mapActions[key].examples.forEach((example) =>
+        console.log("  $ " + example)
+      );
+    });
+});
+
+program.on("command:*", function () {
+  console.error(
+    chalk.red(
+      "Invalid command: %s \nSee --help for a list of available commands."
+    ),
+    program.args.join(" ")
+  );
+  process.exit(1);
+});
+
+program.parse(process.argv);
